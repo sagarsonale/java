8eb9b000ebbc63ac3f44959ee270a9b3b7960cab
sagarsonale <botmate10@gmail.com>
2023-03-03 22:34:52 +0700

    done

---
+++ light.java
@@ -0,0 +1,15 @@
+class light
+{
+    public static void main(String[] args)
+    {
+        int lightspeed;
+        long days,seconds,distance;
+        lightspeed=18600;
+        days=1000;
+        seconds=days*24*60*60;
+        distance=lightspeed*seconds;
+        System.out.println("in days"+days);
+        System.out.println("days light will tarvel about");
+        System.out.println(distance+"miles");
+    }
+}
