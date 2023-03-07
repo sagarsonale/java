ed1727f8097d594979a375872782a5a6cde93ed3
sagarsonale <botmate10@gmail.com>
2023-03-07 15:06:11 +0700

    done

---
+++ reference.java
@@ -0,0 +1,10 @@
+import java.awt.Point;
+class reference
+{
+    public static void main(String[] args)
+    {
+        int age = 30;
+        Point pointA = new Point(10,20);
+        System.out.println(pointA);
+    }
+}
---
+++ str1.java
@@ -0,0 +1,19 @@
+/*class str
+{
+    public static void main(String[] args)
+    {
+    String name = "sagar sonale";
+    System.out.println(name);
+    }
+}*/
+//concatination of string
+class str1
+{
+    public static void main(String[] args)
+    {
+        String name1="good";
+        String name2="morning";
+        String result=name1+name2;
+        System.out.println(result );
+    }
+}
